da690679452ed1aed8f11aaa428ee70de3337426
predator522 <p47017988@gmail.com>
2025-09-02 14:58:36 +0000

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,5 @@
+module.exports = {
+    botName: "T3RROR XMD",      // Bot name shown in browser info
+    version: "3.2.1",           // Bot version
+    ownerNumber: "233209033293" // Your WhatsApp number (international format, no +)
+}
